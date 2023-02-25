b8f1eae574ad1e192d48b1f818195a13469e404b
vdv22 <vdv2@mail.ru>
2023-02-25 08:49:07 +0700

    add file altium

--- led/led/main.c
+++ led/led/main.c
@@ -1,16 +1,9 @@
-/*
- * led.c
- *
- * Created: 25.02.2023 8:38:25
- * Author : Tiger
- */ 
 
-#include <avr/io.h>
-
+#include "main.h"
 
 int main(void)
 {
-    /* Replace with your application code */
+    
     while (1) 
     {
     }
---
+++ led/led/main.h
@@ -0,0 +1,10 @@
+#ifndef MAIN_H_
+#define MAIN_H_
+#include <avr/io.h>
+#include <stdint.h>
+#define F_CPU 16000000
+#include <util/delay.h>
+
+typedef uint8_t u08;
+
+#endif /* MAIN_H_ */
