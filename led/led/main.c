7a8f6a390f01e458bda65997382e09a171c4bd22
vdv22 <vdv2@mail.ru>
2023-02-25 08:39:26 +0700

    add file program

---
+++ led/led/main.c
@@ -0,0 +1,18 @@
+/*
+ * led.c
+ *
+ * Created: 25.02.2023 8:38:25
+ * Author : Tiger
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
